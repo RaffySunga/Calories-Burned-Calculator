9b78cf7729faaf313ebee55c835ba2966988be06
RaffySunga <raphael.sunga090092@my.lsgh.edu.ph>
2021-05-02 17:26:25 +0800

    4 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -18,21 +18,17 @@ export class AboutPage {
   oldmet        : number;
   Activity   : string;
   
+  
 CalculateCaloriesBurned(exercise) {
+ 
 this.Met = exercise;
 this.BurnedCalories=(this.Met * 3.5 * (this.weight/200)) *this.duration;
 this.BurnedCalories = parseFloat(this.BurnedCalories.toFixed(2));
 this.WeightLoss = this.BurnedCalories / 7700;
 this.WeightLoss = parseFloat(this.WeightLoss.toFixed(2));
 
-
-
 this.oldduration=this.duration;
 this.oldweight=this.weight;
-
-
-
-
 
 }
 }
