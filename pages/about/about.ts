b14eab13e822836ec1c5dc3aa8f2170bc047709f
RaffySunga <raphael.sunga090092@my.lsgh.edu.ph>
2021-05-02 13:16:17 +0800

    2 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -8,14 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 
 export class AboutPage {
-  weight: number;
-  duration: number;
-  Met: number;
-  oldweight: number;
-  oldduration: number;
+  weight        : number;
+  duration      : number;
+  Met           : number;
+  oldweight     : number;
+  oldduration   : number;
   BurnedCalories: number;
-  WeightLoss: number;
-  activity; string;
+  WeightLoss    : number;
+  activity      : string;
+  photo_src     : string;
 
 
 CalculateCaloriesBurned(exercise) {
@@ -28,20 +29,26 @@ this.WeightLoss = parseFloat(this.WeightLoss.toFixed(2));
 
 this.oldduration=this.duration;
 this.oldweight=this.weight;
+this.photo_src="https://cdn.jsdelivr.net/gh/RaffySunga/Calories-Burned-Calculator@master/";
 
 if (this.Met=1.3) {
-  this.activity="Sitting at a Desk";}
+  this.activity="Sitting at a Desk";
+  photo_src=this.photo_src+"Sitting.jpg";}
 else if (this.Met=2.2) {
-  this.activity="Washing Dishes"}
+  this.activity="Washing Dishes";
+  photo_src=this.photo_src+"WashingDishes.jpg";}
 else if (this.Met=3.5){
-  this.activity="Light Weight Training"}
+  this.activity="Light Weight Training";
+  this.photo_src=this.photo_src+"LWeight.jpg";}
 else if (this.Met=5.0)
-      {this.activity="Heavy Weight Training"}
+      {this.activity="Heavy Weight Training";
+      this.photo_src=this.photo_src+"HWeight.jpg";}
 else if (this.Met=10){
-     this.activity="Competitive Soccer"}
+     this.activity="Competitive Soccer";
+     this.photo_src=this.photo_src+"Soccer.jpg";}
  else {
-      this.activity="Running"}
+      this.activity="Running";
+      this.photo_src=this.photo_src+"Running.jpg";}
 
-  }
-
+}
 }
